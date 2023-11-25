1c8d7295318e69421251a9566d1200f16e3475c6
phillipefs@msn.com <phillipefs@msn.com>
2023-11-25 13:24:12 +0000

    update class IngestionRawF1

--- utils/class_ingestion_bronze.py
+++ utils/class_ingestion_bronze.py
@@ -6,6 +6,7 @@ from pyspark.sql import DataFrame
 # COMMAND ----------
 
 class IngestionRawF1:
+    
     def __init__(self, spark, dir_file:str, format_file:str, table_name:str, 
                  keys_merge:list, schema_file:str=None, options_file:dict=None):
         self.spark = spark
@@ -30,21 +31,21 @@ class IngestionRawF1:
             df_raw = self.spark.read.format(self.format_file).options(**self.options_file).load(path_read)
         return df_raw
 
-    def merge_into_raw(self,df_raw):
+    def merge_into_raw(self, df_raw: DataFrame)-> None:
         if spark.catalog.tableExists(f"{self.database_raw}.{self.table_name}"):
+            conditions_merge = " and ".join(f"source.{key} = target.{key}" for key in self.keys_merge)
             df_raw.createOrReplaceTempView("source")
             spark.sql(
                 f"""
                 MERGE INTO {self.database_raw}.{self.table_name} as target
                     USING source
-                    on source.constructorId = target.constructorId
+                    on {conditions_merge}
                     WHEN MATCHED THEN 
                         UPDATE SET *
                     WHEN NOT MATCHED THEN
                         INSERT *
                 """
             )
-            print("Table Exist")
         else:
             df_raw.write.format("delta").saveAsTable(f"{self.database_raw}.{self.table_name}")
 
@@ -60,7 +61,7 @@ f1 = IngestionRawF1(
     dir_file="dbfs:/mnt/layer-bronze/udemy-databricks/raw/constructors.json",
     format_file="json",
     table_name="constructors",
-    keys_merge=None,
+    keys_merge=["constructorId"],
     options_file=None,
     schema_file=None
 )
@@ -68,129 +69,96 @@ f1.start_ingestion_raw()
 
 # COMMAND ----------
 
-dbutils.fs.ls("/mnt/layer-bronze/udemy-databricks/raw")
-
-# COMMAND ----------
-
-# Instancia a classe IngestionRawF1
-f1 = IngestionRawF1(spark)
-
-# Define o esquema para o arquivo CSV
-custom_schema = "id int, n int, n1 int, n2 int, number string, number2 string"
-
-# Chama o método df_raw passando o esquema
-f1.df_raw(
-    format_file="json",
-    dir_file="dbfs:/mnt/layer-bronze/udemy-databricks/raw/constructors.json",
-    options={},
-    schema=custom_schema
-)
-
-# COMMAND ----------
-
-f1 = IngestionRawF1(spark)
-
-# COMMAND ----------
-
 list_files = [
     {
-        "dir_file":"/mnt/layer-bronze/udemy-databricks/raw/circuits.csv",
-        "file_format":"csv",
-        "options":{"header":True, "inferSchema":True}
+        "dir_file": "/mnt/layer-bronze/udemy-databricks/raw/constructors.json",
+        "file_format": "json",
+        "table_name": "constructors",
+        "keys_merge": ["constructorId"],
     },
     {
-        "dir_file":"/mnt/layer-bronze/udemy-databricks/raw/drivers.json",
-        "file_format":"json"
+        "dir_file": "/mnt/layer-bronze/udemy-databricks/raw/circuits.csv",
+        "file_format": "csv",
+        "options": {"header": True, "inferSchema": True},
+        "table_name": "circuits",
+        "keys_merge": ["circuitId"]
+    },
+    {
+        "dir_file": "/mnt/layer-bronze/udemy-databricks/raw/races.csv",
+        "file_format": "csv",
+        "options": {"header": True, "inferSchema": True},
+        "table_name": "races",
+        "keys_merge": ["raceId"]
+    },
+    {
+        "dir_file": "/mnt/layer-bronze/udemy-databricks/raw/drivers.json",
+        "file_format": "json",
+        "table_name": "drivers",
+        "keys_merge": ["driverId"],
     },
 ]
 
 # COMMAND ----------
 
 for files in list_files:
-    f1.df_raw(
-        dir_file=    files.get("dir_file"),
+    f1 = IngestionRawF1(
+        spark      = spark,
+        dir_file   = files.get("dir_file"),
         format_file= files.get("file_format"),
-        options=     files.get("options", {}),
-        schema=      files.get("schema")
+        table_name = files.get("table_name"),
+        keys_merge = files.get("keys_merge"),
+        options_file = files.get("options", {}),
+        schema_file = files.get("schema")
     )
+    f1.start_ingestion_raw()
 
 # COMMAND ----------
 
-tables = spark.sql(f"show tables in airlines_bronze")
+from concurrent.futures import ThreadPoolExecutor
 
-# COMMAND ----------
+def process_file(file_info):
+    f1 = IngestionRawF1(
+        spark=spark,
+        dir_file=file_info.get("dir_file"),
+        format_file=file_info.get("file_format"),
+        table_name=file_info.get("table_name"),
+        keys_merge=file_info.get("keys_merge"),
+        options_file=file_info.get("options", {}),
+        schema_file=file_info.get("schema")
+    )
+    f1.start_ingestion_raw()
 
-spark.catalog.tableExists("airlines_bronze.air_cia")
+# Lista de arquivos
+list_files = [
+    {
+        "dir_file": "/mnt/layer-bronze/udemy-databricks/raw/constructors.json",
+        "file_format": "json",
+        "table_name": "constructors",
+        "keys_merge": ["constructorId"],
+    },
+    {
+        "dir_file": "/mnt/layer-bronze/udemy-databricks/raw/circuits.csv",
+        "file_format": "csv",
+        "options": {"header": True, "inferSchema": True},
+        "table_name": "circuits",
+        "keys_merge": ["circuitId"]
+    },
+    {
+        "dir_file": "/mnt/layer-bronze/udemy-databricks/raw/races.csv",
+        "file_format": "csv",
+        "options": {"header": True, "inferSchema": True},
+        "table_name": "races",
+        "keys_merge": ["raceId"]
+    },
+    {
+        "dir_file": "/mnt/layer-bronze/udemy-databricks/raw/drivers.json",
+        "file_format": "json",
+        "table_name": "drivers",
+        "keys_merge": ["driverId"],
+    },
+]
 
-# COMMAND ----------
+# Iniciar threads para processar cada arquivo em paralelo usando ThreadPoolExecutor
+with ThreadPoolExecutor(max_workers=4) as executor:
+    executor.map(process_file, list_files)
 
-# Lista de elementos
-elementos = ['id', 'cpf', 'nome', 'endereco', 'telefone']
-
-# Criando a string de forma dinâmica
-condicoes = " and ".join(f"source.{elemento} = target.{elemento}" for elemento in elementos)
-
-# Exibindo a string resultante
-print(condicoes)
-
-
-# COMMAND ----------
-
-print(" X ".join(list((f"source.{elemento} = target.{elemento}" for elemento in elementos))))
-
-# COMMAND ----------
-
-df_columns = ['id', 'cpf', 'nome', 'endereco', 'telefone']
-
-column_string = ''
-column_string_merge = ''
-for value in df_columns:
-    if value == df_columns[-1]:
-        column_string += value
-        column_string_merge += 'src.' + value
-    else:
-        column_string += value + ', '
-        column_string_merge += 'src.' + value + ', '
-
-print(column_string_merge)
-
-# COMMAND ----------
-
-df_estrutura = ['id', 'cpf', 'nome', 'endereco', 'telefone']
-columns_merge = "id|cpf"
-
-column_string = ''
-column_string_merge = ''
-for value in df_estrutura:
-    if value == df_estrutura[-1]:
-        column_string += value
-        column_string_merge += 'src.' + value
-    else:
-        column_string += value + ', '
-        column_string_merge += 'src.' + value + ', '
-        
-colunas, chave_fmt, flag = '', '', False
-arr_chaves = columns_merge.split("|")
-for x in df_estrutura:
-    for chave in arr_chaves:            
-        find = colunas.find(x)
-        if chave != "" and flag == False:
-            chave_fmt = f"{chave_fmt} and src.{chave} = dtn.{chave}"
-            print(chave_fmt)
-        elif chave != x and chave != '' and find == -1:  
-            colunas += f'dtn.{x} = src.{x} ,'    
-    flag = True
-cond_merge = chave_fmt[5:]
-colunas = colunas.replace(',dtn.update_date = src.update_date','')
-
-
-
-
-# COMMAND ----------
-
-df_columns = ['id', 'cpf', 'nome', 'endereco', 'telefone']
-columns_merge = "id|cpf"
-
-keys_columns = columns_merge.split("|")
-
-print(", ".join(f"source.{column} = target.{column}" for column in keys_columns))
