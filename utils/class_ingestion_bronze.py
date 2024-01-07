b0d132e902c64ef09676865f1294df2af9c1d7a8
phillipefs@msn.com <phillipefs@msn.com>
2024-01-07 13:32:41 +0000

    add try exception in functions

--- utils/class_ingestion_bronze.py
+++ utils/class_ingestion_bronze.py
@@ -20,35 +20,42 @@ class IngestionRawF1:
         self.database_raw = "analytics_f1_bronze"
 
     def df_raw(self) -> DataFrame:
-        if self.format_file in self.dir_file:
-            path_read = self.dir_file
-        else:
-            path_read = self.dir_file + f"/*.{self.format_file}"
+        try:
+            if self.format_file in self.dir_file:
+                path_read = self.dir_file
+            else:
+                path_read = self.dir_file + f"/*.{self.format_file}"
 
-        if self.schema_file:
-            df_raw = self.spark.read.format(self.format_file).schema(self.schema_file).options(**self.options_file).load(path_read)
-        else:
-            df_raw = self.spark.read.format(self.format_file).options(**self.options_file).load(path_read)
-        return df_raw
+            if self.schema_file:
+                df_raw = self.spark.read.format(self.format_file).schema(self.schema_file).options(**self.options_file).load(path_read)
+            else:
+                df_raw = self.spark.read.format(self.format_file).options(**self.options_file).load(path_read)
+            
+            return df_raw
+        except Exception as e:
+            print(f"Error in df_raw(): {str(e)}")
 
     def merge_into_raw(self, df_raw: DataFrame)-> None:
-        if spark.catalog.tableExists(f"{self.database_raw}.{self.table_name}"):
-            print(self.table_name)
-            conditions_merge = " and ".join(f"source.{key} = target.{key}" for key in self.keys_merge)
-            df_raw.createOrReplaceTempView("source")
-            spark.sql(
-                f"""
-                MERGE INTO {self.database_raw}.{self.table_name} as target
-                    USING source
-                    on {conditions_merge}
-                    WHEN MATCHED THEN 
-                        UPDATE SET *
-                    WHEN NOT MATCHED THEN
-                        INSERT *
-                """
-            )
-        else:
-            df_raw.write.format("delta").saveAsTable(f"{self.database_raw}.{self.table_name}")
+        try:
+            if spark.catalog.tableExists(f"{self.database_raw}.{self.table_name}"):
+                print(self.table_name)
+                conditions_merge = " and ".join(f"source.{key} = target.{key}" for key in self.keys_merge)
+                df_raw.createOrReplaceTempView("source")
+                spark.sql(
+                    f"""
+                    MERGE INTO {self.database_raw}.{self.table_name} as target
+                        USING source
+                        on {conditions_merge}
+                        WHEN MATCHED THEN 
+                            UPDATE SET *
+                        WHEN NOT MATCHED THEN
+                            INSERT *
+                    """
+                )
+            else:
+                df_raw.write.format("delta").saveAsTable(f"{self.database_raw}.{self.table_name}")
+        except Exception as e:
+            print(f"Error in merge_into_raw(): {str(e)}")
 
     def start_ingestion_raw(self)->None:
         df = self.df_raw()
