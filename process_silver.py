96a51b07052b5241bfdd913ab2d5faaf57c55968
phillipefs@msn.com <phillipefs@msn.com>
2024-01-08 23:58:26 +0000

    create parametes

--- process_silver.py
+++ process_silver.py
@@ -6,13 +6,21 @@ from pyspark.sql import DataFrame
 
 # COMMAND ----------
 
+# DBTITLE 1,Parameters
+dbutils.widgets.text("db_bronze", "")
+dbutils.widgets.text("db_silver", "")
+db_bronze = dbutils.widgets.get("db_bronze")
+db_silver = dbutils.widgets.get("db_silver")
+
+# COMMAND ----------
+
 # DBTITLE 1,Import Common Functions
 # MAGIC %run ./utils/common_functions
 
 # COMMAND ----------
 
 # DBTITLE 1,Load Circuits
-df_circuits = spark.read.format("delta").table("analytics_f1_bronze.circuits")
+df_circuits = spark.read.format("delta").table(f"{db_bronze}.circuits")
 df_circuits = df_circuits\
     .select(
         col("circuitId").alias("circuit_id"),
@@ -25,13 +33,13 @@ df_circuits = df_circuits\
         col("alt").alias("altitude"))
 
 df_circuits = add_ingestion_date(df_circuits)
-df_circuits.write.format("delta").mode("overwrite").saveAsTable("analytics_f1_silver.circuits")
+df_circuits.write.format("delta").mode("overwrite").saveAsTable(f"{db_silver}.circuits")
         
 
 # COMMAND ----------
 
 # DBTITLE 1,Load Race
-df_race = spark.read.format("delta").table("analytics_f1_bronze.races")
+df_race = spark.read.format("delta").table(f"{db_bronze}.races")
 df_race = df_race\
     .withColumn("race_timestamp", to_timestamp(concat(col("date"), lit(" "), col("time")), "yyyy-MM-dd HH:mm:ss"))\
     .select(
@@ -43,12 +51,12 @@ df_race = df_race\
         col("race_timestamp"))
     
 df_race = add_ingestion_date(df_race)
-df_race.write.format("delta").mode("overwrite").saveAsTable("analytics_f1_silver.races")
+df_race.write.format("delta").mode("overwrite").saveAsTable(f"{db_silver}.races")
 
 # COMMAND ----------
 
 # DBTITLE 1,Load Constructors
-df_constructors = spark.read.format("delta").table("analytics_f1_bronze.constructors")
+df_constructors = spark.read.format("delta").table(f"{db_bronze}.constructors")
 
 df_constructors = df_constructors\
     .select(
@@ -56,15 +64,15 @@ df_constructors = df_constructors\
         col("constructorRef").alias("constructor_ref"),
         col("name"),
         col("nationality"))
-
+    
 df_constructors = add_ingestion_date(df_constructors)
-df_constructors.write.format("delta").mode("overwrite").saveAsTable("analytics_f1_silver.constructors")
+df_constructors.write.format("delta").mode("overwrite").saveAsTable(f"{db_silver}.constructors")
     
 
 # COMMAND ----------
 
 # DBTITLE 1,Load Drivers
-df_drives = spark.read.format("delta").table("analytics_f1_bronze.drivers")
+df_drives = spark.read.format("delta").table(f"{db_bronze}.drivers")
 
 df_drivers = df_drives\
   .select(
@@ -78,13 +86,13 @@ df_drivers = df_drives\
    .replace("", None)
 
 df_drivers = add_ingestion_date(df_drivers)
-df_drives.write.format("delta").option("mergeSchema", "true").mode("overwrite").saveAsTable("analytics_f1_silver.drivers")
+df_drives.write.format("delta").option("mergeSchema", "true").mode("overwrite").saveAsTable(f"{db_silver}.drivers")
 
 
 # COMMAND ----------
 
 # DBTITLE 1,Load Results
-df_results = spark.read.format("delta").table("analytics_f1_bronze.results")
+df_results = spark.read.format("delta").table(f"{db_bronze}.results")
 
 df_results = df_results\
     .select(
@@ -106,12 +114,12 @@ df_results = df_results\
         col("rank").cast("int"))
 
 df_results = add_ingestion_date(df_results)
-df_results.write.format("delta").option("mergeSchema", "true").mode("overwrite").saveAsTable("analytics_f1_silver.results")
+df_results.write.format("delta").option("mergeSchema", "true").mode("overwrite").saveAsTable(f"{db_silver}.results")
 
 # COMMAND ----------
 
 # DBTITLE 1,Load Pit Stops
-df_pit_stops = spark.read.format("delta").table("analytics_f1_bronze.pit_stops")
+df_pit_stops = spark.read.format("delta").table(f"{db_bronze}.pit_stops")
 
 df_pit_stops\
     .select(
@@ -124,12 +132,12 @@ df_pit_stops\
         col("time"))
 
 df_pit_stops = add_ingestion_date(df_pit_stops)
-df_pit_stops.write.format("delta").mode("overwrite").saveAsTable("analytics_f1_silver.pit_stops")
+df_pit_stops.write.format("delta").mode("overwrite").saveAsTable(f"{db_silver}.pit_stops")
 
 # COMMAND ----------
 
 # DBTITLE 1,Load Lap Times
-df_lap_times = spark.read.format("delta").table("analytics_f1_bronze.lap_times")
+df_lap_times = spark.read.format("delta").table(f"{db_bronze}.lap_times")
 df_lap_times = df_lap_times\
     .select(
         col("raceId").alias("race_id"),
@@ -140,13 +148,13 @@ df_lap_times = df_lap_times\
         col("milliseconds"))
 
 df_lap_times = add_ingestion_date(df_lap_times)
-df_lap_times.write.format("delta").mode("overwrite").saveAsTable("analytics_f1_silver.lap_times")
+df_lap_times.write.format("delta").mode("overwrite").saveAsTable(f"{db_silver}.lap_times")
 
 
 # COMMAND ----------
 
 # DBTITLE 1,Load Qualifying
-df_qualifying = spark.read.format("delta").table("analytics_f1_bronze.qualifying")
+df_qualifying = spark.read.format("delta").table(f"{db_bronze}.qualifying")
 
 df_qualifying\
     .select(
@@ -161,4 +169,4 @@ df_qualifying\
         col("q3"))
 
 df_qualifying = add_ingestion_date(df_qualifying)
-df_qualifying.write.format("delta").mode("overwrite").saveAsTable("analytics_f1_silver.qualifying")
+df_qualifying.write.format("delta").mode("overwrite").saveAsTable(f"{db_silver}.qualifying")
